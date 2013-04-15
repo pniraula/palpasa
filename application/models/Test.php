2f293502ba8fbaf2ea600f53ecffb1d9f3a8a248
pniraula <prabeshniraula@gmail.com>
2013-04-15 19:15:46 -0400

    added ci framework related stufs

---
+++ application/models/Test.php
@@ -0,0 +1,12 @@
+<?php
+	/**
+	 * 
+	 */
+	class Test extends CI_Model {
+		
+		public function show(){
+			return "I am from the model";
+		}
+	}
+	
+?>
---
+++ application/views/view_home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Palpasa | <?=$title?></title>
+	
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to my site</h1>
+	<div>
+		<?=$content?>
+		<a href="about">about</a>
+	</div>
+</div>
+
+</body>
+</html>
